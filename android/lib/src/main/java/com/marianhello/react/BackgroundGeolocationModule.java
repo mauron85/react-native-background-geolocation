753238ea3c17ef7cdf7faec436f6cb2e4fc7bffe
farruxx <farruxx@users.noreply.github.com>
2017-06-10 18:06:38 +0500

    Read from config syncUrl param

--- android/lib/src/main/java/com/marianhello/react/BackgroundGeolocationModule.java
+++ android/lib/src/main/java/com/marianhello/react/BackgroundGeolocationModule.java
@@ -239,6 +239,7 @@ public class BackgroundGeolocationModule extends ReactContextBaseJavaModule impl
         if (options.hasKey("activitiesInterval")) config.setActivitiesInterval(options.getInt("activitiesInterval"));
         if (options.hasKey("stopOnStillActivity")) config.setStopOnStillActivity(options.getBoolean("stopOnStillActivity"));
         if (options.hasKey("url")) config.setUrl(options.getString("url"));
+        if (options.hasKey("syncUrl")) config.setSyncUrl(options.getString("syncUrl"));
         if (options.hasKey("httpHeaders")) {
             HashMap httpHeaders = new HashMap<String, String>();
             ReadableMap rm = options.getMap("httpHeaders");
