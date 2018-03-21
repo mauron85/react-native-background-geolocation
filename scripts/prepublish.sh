2dedb03570c9e4e64929f01a8cf90e9591f30245
Marian Hello <marian.hello@gmail.com>
2018-03-21 08:42:10 +0100

    Android revert changes that broke react-native link

--- scripts/prepublish.sh
+++ scripts/prepublish.sh
@@ -3,4 +3,6 @@
 git submodule init
 git submodule update
 
-cp -r android/common/src/main/* android/lib/src/main/
+mkdir -p android/lib/src/main/res/
+cp -r android/common/src/main/java/* android/lib/src/main/java/
+cp -r android/common/src/main/res/* android/lib/src/main/res/
