cc518051b7a88761879254d4875c8a71c57e711e
Nikita Makarov <nik.1em.xyz@gmail.com>
2018-04-02 19:26:37 +0300

    chain commands

--- scripts/prepublish/script.sh
+++ scripts/prepublish/script.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-git submodule init
-git submodule update
-
 mkdir -p android/lib/src/main/res/
-cp -r android/common/src/main/java/* android/lib/src/main/java/
-cp -r android/common/src/main/res/* android/lib/src/main/res/
+
+git submodule init && git submodule update && cp -r android/common/src/main/java/* android/lib/src/main/java/ && cp -r android/common/src/main/res/* android/lib/src/main/res/
