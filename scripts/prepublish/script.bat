df15f17d1cb384e0179f288ce03058283ae0cd88
Nikita Makarov <nik.1em.xyz@gmail.com>
2018-04-02 19:00:08 +0300

    remove git submodules autopulling

--- scripts/prepublish/script.bat
+++ scripts/prepublish/script.bat
@@ -1,6 +1,5 @@
 git.exe submodule init
 git.exe submodule update
-git.exe submodule foreach git pull
 
 md android\lib\src\main\res\
 robocopy %CD%\android\common\src\main\java\ %CD%\android\lib\src\main\java\ /E
--- scripts/prepublish/script.sh
+++ scripts/prepublish/script.sh
@@ -2,7 +2,6 @@
 
 git submodule init
 git submodule update
-git submodule foreach git pull
 
 mkdir -p android/lib/src/main/res/
 cp -r android/common/src/main/java/* android/lib/src/main/java/
