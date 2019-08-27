c5aa0436b903c55de046fb39de7f270ec6b1461e
Marian Hello <m.hello@gmail.com>
2019-08-27 20:18:44 +0200

    fix tests

--- android/lib/src/test/java/com/marianhello/bgloc/react/ConfigMapperTest.java
+++ android/lib/src/test/java/com/marianhello/bgloc/react/ConfigMapperTest.java
@@ -9,6 +9,7 @@ import com.marianhello.bgloc.Config;
 import junit.framework.Assert;
 
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.invocation.InvocationOnMock;
