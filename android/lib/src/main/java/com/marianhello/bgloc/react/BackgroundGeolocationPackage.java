e22401ab56926be9f93f096dc21d936bcb1fb679
Marian Hello <m.hello@gmail.com>
2019-08-29 19:20:22 +0200

    remove deprecated react-native methods

--- android/lib/src/main/java/com/marianhello/bgloc/react/BackgroundGeolocationPackage.java
+++ android/lib/src/main/java/com/marianhello/bgloc/react/BackgroundGeolocationPackage.java
@@ -19,11 +19,6 @@ public class BackgroundGeolocationPackage implements ReactPackage {
       return modules;
   }
 
-  // Deprecated RN 0.47
-  public List<Class<? extends JavaScriptModule>> createJSModules() {
-      return Collections.emptyList();
-  }
-
   @Override
   public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
       return Collections.emptyList();
